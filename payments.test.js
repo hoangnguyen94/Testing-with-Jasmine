e5464a25de7c723b9a75ed01cc8bfd2d57f1dbec
henry <nguyentuanhoang1994@yahoo.com>
2022-02-15 23:07:31 -0800

    update payment testing script

--- payments.test.js
+++ payments.test.js
@@ -1,5 +1,6 @@
-describe('Payment test "with setup and tear"', function() {
-beforeEach( function () {
+describe('Payment test "with setup and tear-down"', function() {
+// input value for the test function
+    beforeEach( function () {
     billAmtInput.value = 100;
     tipAmtInput.value = 20;
 });
@@ -11,11 +12,13 @@ it('should add a new payment to allPayments on submitPaymentInfo()', function ()
     expect(allPayments['payment1'].tipAmt).toEqual('20');
     expect(allPayments['payment1'].tipPercent).toEqual(20);
   });
+// create test function to make sure the payment won't add with empty input
 it('should not add new payment on submitPaymentInfo() with empty input', function () {
     billAmtInput.value = '';
     submitPaymentInfo();
     expect(Object.keys(allPayments).length).toEqual(0);
 });
+// create test function to see the payment update
 it('should payment update #paymentTable on appendPaymentTable()', function () {
     let curPayment = createCurPayment();
     allPayments['payment1'] = curPayment;
@@ -26,6 +29,7 @@ it('should payment update #paymentTable on appendPaymentTable()', function () {
     expect(curList[1].innerText).toEqual('$20');
     expect(curList[2].innerText).toEqual('20%');
 });
+// create test function to see how the new payment create
 it('should create a new payment on createCurPayment()', function () {
     let expectPayment = {
         billAmt: "100",
@@ -34,6 +38,7 @@ it('should create a new payment on createCurPayment()', function () {
     }
     expect(createCurPayment()).toEqual(expectPayment);
 });
+// create test function to see is the payment not create with empty input
 it('should not create payment with empty input on createCurPayment()', function () {
     billAmtInput.value = "";
     tipAmtInput.value = "";
@@ -41,7 +46,7 @@ it('should not create payment with empty input on createCurPayment()', function 
 
     expect(curPayment).toEqual(undefined);
 });
-
+// to reset the test function after done test
 afterEach(function () {
     billAmtInput.value = "";
     tipAmtInput.value = "";
